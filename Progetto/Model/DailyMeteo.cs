4a7fc2857c4109ee0555e0326169bbfb5dae40af
MarcoPassoni <91603717+MarcoPassoni@users.noreply.github.com>
2023-03-31 23:00:42 +0200

    update

--- Progetto/Model/DailyMeteo.cs
+++ Progetto/Model/DailyMeteo.cs
@@ -26,7 +26,9 @@ namespace Progetto.Model
             Windgusts10mMin = windgusts10mMin;
             Winddirection10mDominant = winddirection10mDominant;
             hourlies = hourly;
-    }
+            TrovaTempo();
+        }
+        public string Weather { get; set; }
         public List<Ore> hourlies { get; set; }
         public DateTime Time { get; set; }
         public int Weathercode { get; set; }
@@ -41,6 +43,42 @@ namespace Progetto.Model
         public double Windgusts10mMin { get; set; }
         public int Winddirection10mDominant { get; set; }
 
+        public void TrovaTempo()
+        {
+            int? code = Weathercode;
+            Weather = code switch
+            {
+                0 => "cielo sereno",
+                1 => "prevalentemente limpido",
+                2 => "parzialmente nuvoloso",
+                3 => "coperto",
+                45 => "nebbia",
+                48 => "nebbia con brina",
+                51 => "pioggerellina di scarsa intensità",
+                53 => "pioggerellina di moderata intensità",
+                55 => "pioggerellina intensa",
+                56 => "pioggerellina gelata di scarsa intensità",
+                57 => "pioggerellina gelata intensa",
+                61 => "pioggia di scarsa intensità",
+                63 => "pioggia di moderata intensità",
+                65 => "pioggia molto intensa",
+                66 => "pioggia gelata di scarsa intensità",
+                67 => "pioggia gelata intensa",
+                71 => "venicata di lieve entità",
+                73 => "nevicata di media entità",
+                75 => "nevicata intensa",
+                77 => "granelli di neve",
+                80 => "deboli rovesci di pioggia",
+                81 => "moderati rovesci di pioggia",
+                82 => "violenti rovesci di pioggia",
+                85 => "leggeri rovesci di neve",
+                86 => "pesanti rovesci di neve",
+                95 => "temporale lieve o moderato",
+                96 => "temporale con lieve grandine",
+                99 => "temporale con forte grandine",
+                _ => string.Empty,
+            };
+        }
 
         private DateTime UnixTimeStampToDateTime(int unixTimeStamp)
         {
